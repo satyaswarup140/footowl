270bba0b2c841b6c6ea8b3a321c3a81a2590e330
SATYASWARUP DAS <153348365+satyaswarup140@users.noreply.github.com>
2024-12-09 09:03:21 +0530

    Update test-sequelize.js

--- library_system/test-sequelize.js
+++ library_system/test-sequelize.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'library_system',
   process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || 'S1992@das#',
+  process.env.DB_PASSWORD || '*******',
   {
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
