705e819c8f73ea18e87d5d6f84bd50243a004ca3
SATYASWARUP DAS <153348365+satyaswarup140@users.noreply.github.com>
2024-12-09 09:04:31 +0530

    Update config.js

--- library_system/config/config.js
+++ library_system/config/config.js
@@ -6,21 +6,21 @@ console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 module.exports = {
     development: {
       username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'S1992@das#',
+      password: process.env.DB_PASSWORD || '********',
       database: process.env.DB_NAME,
       host: process.env.DB_HOST,
       dialect: 'mysql',
     },
     test: {
       username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'S1992@das#',
+      password: process.env.DB_PASSWORD || '*******',
       database: process.env.DB_NAME,
       host: process.env.DB_HOST,
       dialect: 'mysql',
     },
     production: {
       username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'S1992@das#',        
+      password: process.env.DB_PASSWORD || '*******',        
       database: process.env.DB_NAME,
       host: process.env.DB_HOST,
       dialect: 'mysql',
